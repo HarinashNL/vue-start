bc93a907712004fd5b3063eb598a1047864911a2
Harinash <prohari35@gmail.com>
2024-05-12 22:24:41 +0800

    edit page not working

--- src/views/PagesList.vue
+++ src/views/PagesList.vue
@@ -5,7 +5,7 @@
       >New Page
     </router-link>
   </div>
-  <table class="table table-striped table-hober">
+  <table class="table table-hober">
     <thead>
       <tr>
         <th>Title</th>
@@ -14,7 +14,11 @@
       </tr>
     </thead>
     <tbody>
-      <tr v-for="(page, index) in $pages.getAllPages()" :key="index">
+      <tr
+        v-for="(page, index) in $pages.getAllPages()"
+        :key="index"
+        @click="goToPage(index)"
+      >
         <td>{{ page.pageTitle }}</td>
         <td>{{ page.link.text }}</td>
         <td>{{ page.published ? "yes" : "no" }}</td>
@@ -27,14 +31,19 @@
 
 <script setup>
 import { ref, reactive, inject } from "vue";
-
+import { useRouter } from "vue-router";
 //const counter = ref(0); //{value:0}
-const data = reactive({ counter: 0 });
+// const data = reactive({ counter: 0 });
 const $pages = inject("$pages");
+const router = useRouter();
 
 function click() {
   //   counter.value++;
   data.counter++;
+}
+
+function goToPage(index) {
+  router.push({ path: `pages/${index}/edit` });
 }
 
 // export default {
@@ -50,3 +59,9 @@ function click() {
 //   },
 // };
 </script>
+
+<style scope>
+.table.table-hover tr:hover {
+  cursor: pointer;
+}
+</style>
